c5aeb182da08175c71db708966a99aeb125292d9
sanjayfirmansyah <sanjayfirmanzyah@gmail.com>
2026-06-09 11:50:12 +0700

    feat: implement distributor module with SAP synchronization and API routing

--- app/Modules/Distributor/Routes/api.php
+++ app/Modules/Distributor/Routes/api.php
@@ -8,5 +8,5 @@ Route::prefix('v1/distributors')->middleware('auth:sanctum')->group(function () 
     Route::post('/sync', [DistributorController::class, 'sync']);
     Route::get('/addresses', [DistributorController::class, 'getAddresses']);
     Route::get('/ocr-codes', [DistributorController::class, 'getOcrCodes']);
-    Route::get('/{id}', [DistributorController::class, 'show']);
+    Route::get('/{id}', [DistributorController::class, 'show'])->whereNumber('id');
 });
