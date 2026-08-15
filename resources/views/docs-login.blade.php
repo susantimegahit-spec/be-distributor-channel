cce403813657b0b4e14395c2e0db3ce542f8cf8b
sanjayfirmansyah <sanjayfirmanzyah@gmail.com>
2026-08-15 11:54:33 +0700

    feat: add CustomerShipto bulk upload functionality and API documentation

--- resources/views/docs-login.blade.php
+++ resources/views/docs-login.blade.php
@@ -414,11 +414,11 @@
                 </button>
             </form>
 
-            <!-- Bottom Brand Info -->
-            <a href="{{ url('/') }}" class="bottom-brand">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
-                <span>PT Susanti Megah • SMESTA API</span>
-            </a>
+            <!-- Bottom Brand Info (Private Access) -->
+            <div class="bottom-brand" style="cursor: default;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
+                <span>PT Susanti Megah • Confidential & Proprietary System</span>
+            </div>
 
         </div>
 
