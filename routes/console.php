cd415d3fd84a610915d162868dc85407c6cdd531
sanjayfirmansyah <42969670+Sanjayfirmansyah@users.noreply.github.com>
2026-09-02 15:51:43 +0700

    sync

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,12 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
+// Frequent Status Sync
 Schedule::command('sap:sync-pdo-status')->everyTenMinutes();
 Schedule::command('sap:sync-order-status')->everyTenMinutes();
+
+// Daily Master Data Sync from SAP B1
+Schedule::command('sap:sync-discount-types')->daily();
+Schedule::command('sap:sync-sales-employees')->daily();
+Schedule::command('sap:sync-warehouses')->daily();
+Schedule::command('sap:sync-ocr-codes')->daily();
